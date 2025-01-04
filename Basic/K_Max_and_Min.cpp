ecc01e3a73ce83efc8feb0d2b2f00a113190210e
rahulgtst <rahulgtst@gmail.com>
2025-01-05 00:22:26 +0530

    Update the Max_and_Min file code

--- Basic/K_Max_and_Min.cpp
+++ Basic/K_Max_and_Min.cpp
@@ -8,18 +8,17 @@ using namespace std;
 int main() {
     int a, b, c;
     cin >> a >> b >> c;
-    if(a > b) {
-        if(b > c) {
-            cout << c << " " << a;
-        } else {
-            cout << b << " " << a;
-        }
-    } else {
-        if(a > c) {
-            cout << c << " " << b;
-        } else {
-            cout << a << " " << b;
-        }
+    if(a >= b && a >= c) {
+        if(b >= c) cout << c << " " << a;
+        else cout << b << " " << a;
+    }
+    else if(b >= a && b >= c) {
+        if(a >= c) cout << c << " " << b;
+        else cout << a << " " << b;
+    } 
+    else if(c >= b && c >= a) {
+        if(b >= a) cout << a << " " << c;
+        else cout << b << " " << c;
     }
     return 0;
 }
